fae0ea5c17263edf09c51e67b8a9f19b92a393ba
Chia-Hsuan Chang <sham82503@gmail.com>
2022-10-06 16:03:49 +0800

    decouple mongodb operation from parallel function

--- retrieve_pages.py
+++ retrieve_pages.py
@@ -1,13 +1,9 @@
 import requests
 import time
-import sys
-import glob
-import math
-import re
 import tqdm
 import argparse
 import configparser
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 from pymongo import MongoClient
 
 def get_collection_cursor():
@@ -49,147 +45,126 @@ def parse_response(j):
 
     return continue_flag, tmp
 
-def retrieve_pages(list_of_categories):
-    """ this function retrieves Wikipedia comparable articles (pages) from a list of categories and has following steps
+def retrieve_pages(in_tuple):
+    """ this function retrieves Wikipedia comparable articles (pages) from a category and has following steps
     (1) find pages from a category
     (2) loop each page from pages
-    (3) when there doesn't exist an English page in database
-    (4) find its Chinese version
-    (5) fetch abstracts for both English and Chinese pages
+    (3) find its Chinese version
+    (4) fetch abstracts for both English and Chinese pages
 
     Args:
-        list_of_categories (list): a list of tuples, where each containing root category, hierarchy, and category
+        in_tuple (list): a tuples containing root category, hierarchy, and category
     """
-    
-    target_collection = get_collection_cursor()
 
     URL = "https://en.wikipedia.org/w/api.php"
     CHINESE_URL = "https://zh.wikipedia.org/w/api.php"
-    
-    for in_tuple in list_of_categories:
-        
-        root_cat, hierarchy, category = in_tuple
+            
+    root_cat, hierarchy, category = in_tuple
 
-        PARAMS = {
-            "action": "query",
-            "generator": "categorymembers",
-            "gcmtitle": "{}".format(category),
-            "gcmlimit": "max",
-            "gcmtype": "page",
-            "gcmcontinue":"",
-            "format": "json"
-        }
+    PARAMS = {
+        "action": "query",
+        "generator": "categorymembers",
+        "gcmtitle": "{}".format(category),
+        "gcmlimit": "max",
+        "gcmtype": "page",
+        "gcmcontinue":"",
+        "format": "json"
+    }
 
-        # store list of tuple(pageid, pagetitle)
-        rs = []
+    # store list of tuple(pageid, pagetitle)
+    rs = []
 
-        # 0 find the pages based on the category
+    # 0 find the pages based on the category
+    R = requests.get(URL, params=PARAMS)
+    continue_flag, tmp = parse_response(R.json())
+    rs += tmp
+
+    # continue_flag is not False when number of returns > 500
+    # as this happens, the program will fill the gcontinue into the PARAMS for getting the information of next page
+    while(continue_flag):
+        PARAMS["gcmcontinue"] = continue_flag
         R = requests.get(URL, params=PARAMS)
         continue_flag, tmp = parse_response(R.json())
         rs += tmp
+    
+    # store list of comparable page pairs
+    data_list = []
 
-        # continue_flag is not False when number of returns > 500
-        # as this happens, the program will fill the gcontinue into the PARAMS for getting the information of next page
-        while(continue_flag):
-            PARAMS["gcmcontinue"] = continue_flag
-            R = requests.get(URL, params=PARAMS)
-            continue_flag, tmp = parse_response(R.json())
-            rs += tmp
-        
-        # process each page
-        for elem in rs:
-            English_pageid = elem[0]
-            English_page_title = elem[1]
+    # process each page
+    for elem in rs:
+        English_pageid = elem[0]
+        English_page_title = elem[1]
             
-            # 1 only process English_pageid which doesn't exist in MongoDB
-            if target_collection.count_documents({"English_pageid":English_pageid}) == 0:
-                
-                LANGLINK_PARAMS = {
-                    "action":"query",
-                    "prop":"langlinks",
-                    "pageids":English_pageid,
-                    "lllang":"zh",
-                    "format":"json"
-                }
+        LANGLINK_PARAMS = {
+            "action":"query",
+            "prop":"langlinks",
+            "pageids":English_pageid,
+            "lllang":"zh",
+            "format":"json"
+        }
 
-                # find its Chinese article
-                R = requests.get(URL, params=LANGLINK_PARAMS)
-                R_json = R.json()
-                # if there is a interlink (Chinese article) of this English_pageid
-                if "langlinks" in R_json["query"]["pages"][str(English_pageid)]:
-                    Chinese_page_title = R_json["query"]["pages"][str(English_pageid)]["langlinks"][0]["*"]
-                
-                    # 3 retrieve abstract of both english page and chinese page
-                    ENGLISH_EXTRACT_PARAMS = {
-                        "action":"query",
-                        "prop":"extracts",
-                        "pageids":English_pageid,
-                        "format":"json",
-                        "explaintext":"true",
-                        "exintro":"true"
-                        # "exlimit":1
-                    }
-
-                    R = requests.get(URL, params=ENGLISH_EXTRACT_PARAMS)
-                    R_json = R.json()
-                    extracted_English_abstract = R_json["query"]["pages"][str(English_pageid)]["extract"]
-
-                    CHINESE_EXTRACT_PARAMS = {
-                        "action":"query",
-                        "prop":"extracts",
-                        "titles":Chinese_page_title,
-                        "format":"json",
-                        "explaintext":"true",
-                        "exintro":"true"
-                        # "exlimit":1
-                    }
-
-                    R = requests.get(CHINESE_URL, params=CHINESE_EXTRACT_PARAMS)
-                    R_json = R.json()
-    
-                    # we yet have Chinese pageid (key for accessing the item), so we must use for to retrieve item
-                    for _, item in R_json["query"]["pages"].items():
-                        if item.get("pageid"):
-                            Chinese_pageid = item["pageid"]
-                            extracted_Chinese_abstract = item["extract"]
-                        else:
-                            Chinese_pageid = ""
-                            extracted_Chinese_abstract = ""
-                        break # only process the first item
-
-                    # 4 store in mongo
-                    # define data template for mongo
-                    data = {
-                        "root_category": root_cat,
-                        "category": category,
-                        "category_hierarchy": hierarchy,
-                        "English_pageid": English_pageid,
-                        "English_page_title": English_page_title,
-                        "extracted_English_abstract":extracted_English_abstract,
-                        "Chinese_pageid": Chinese_pageid,
-                        "Chinese_page_title": Chinese_page_title,
-                        "extracted_Chinese_abstract": extracted_Chinese_abstract
-                    }
-
-                    target_collection.insert_one(data)
+        # find its Chinese article
+        R = requests.get(URL, params=LANGLINK_PARAMS)
+        R_json = R.json()
+        # if there is a interlink (Chinese article) of this English_pageid
+        if "langlinks" in R_json["query"]["pages"][str(English_pageid)]:
+            Chinese_page_title = R_json["query"]["pages"][str(English_pageid)]["langlinks"][0]["*"]
         
-def chunk(sequence, chunk_size):
-    """ this helps create pymongo connection each chunk for improving efficiency
-    rather than create pymongo connection for each post
+            # 3 retrieve abstract of both english page and chinese page
+            ENGLISH_EXTRACT_PARAMS = {
+                "action":"query",
+                "prop":"extracts",
+                "pageids":English_pageid,
+                "format":"json",
+                "explaintext":"true",
+                "exintro":"true"
+                # "exlimit":1
+            }
 
-    Args:
-        sequence (list): list of objects
-        chunk_size (int): size of chunk
+            R = requests.get(URL, params=ENGLISH_EXTRACT_PARAMS)
+            R_json = R.json()
+            extracted_English_abstract = R_json["query"]["pages"][str(English_pageid)]["extract"]
 
-    Yields:
-        list: a list with a determined chunk size
+            CHINESE_EXTRACT_PARAMS = {
+                "action":"query",
+                "prop":"extracts",
+                "titles":Chinese_page_title,
+                "format":"json",
+                "explaintext":"true",
+                "exintro":"true"
+                # "exlimit":1
+            }
 
-    Reference: https://medium.com/@saksham.malhotra2196/the-efficient-way-of-using-multiprocessing-with-pymongo-a7f1cf72b5b7
-    Reference: https://stackoverflow.com/questions/41104582/pymongo-multiprocessing
-    
-    """
-    for j in range(0, len(sequence), chunk_size):
-        yield sequence[j:j + chunk_size]
+            R = requests.get(CHINESE_URL, params=CHINESE_EXTRACT_PARAMS)
+            R_json = R.json()
+
+            # we yet have Chinese pageid (key for accessing the item), so we must use for to retrieve item
+            for _, item in R_json["query"]["pages"].items():
+                if item.get("pageid"):
+                    Chinese_pageid = item["pageid"]
+                    extracted_Chinese_abstract = item["extract"]
+                else:
+                    Chinese_pageid = ""
+                    extracted_Chinese_abstract = ""
+                break # only process the first item
+
+            # 4 store in mongo
+            # define data template for mongo
+            data = {
+                "root_category": root_cat,
+                "category": category,
+                "category_hierarchy": hierarchy,
+                "English_pageid": English_pageid,
+                "English_page_title": English_page_title,
+                "extracted_English_abstract":extracted_English_abstract,
+                "Chinese_pageid": Chinese_pageid,
+                "Chinese_page_title": Chinese_page_title,
+                "extracted_Chinese_abstract": extracted_Chinese_abstract
+            }
+
+            data_list.append(data)
+        
+    return data_list
 
 if __name__ == "__main__":
     
@@ -197,18 +172,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--root', type=str, required=True,
                     help='root of category')
-    parser.add_argument('-c', '--core', type=int, required=True,
-                    help='number of cores')
-    parser.add_argument('-cs', '--chunk_size', type=int, required=True,
-                    help='size of each chunk')
     parser.add_argument('-d', '--depth', type=int, required=True,
                     help="depth limit")
 
     args = parser.parse_args()
     root_cat = args.root
-    core = args.core
-    chunk_size = args.chunk_size
     depth_limit = args.depth
+    core = cpu_count() - 1
 
     # init and read config file - connect to mongodb
     config = configparser.ConfigParser()
@@ -243,11 +213,19 @@ if __name__ == "__main__":
             seen_cats.add(name)
 
     print("Got {} distinct categories after reading.".format(len(cats)))
-
+    
     pool = Pool(processes=core)
-    for _ in tqdm.tqdm(pool.imap_unordered(retrieve_pages, chunk(cats, chunk_size)),
-     total=math.ceil(len(cats)/chunk_size)):
-        pass # use for loop to trigger the tqdm
+    pbar = tqdm.tqdm(total=len(cats))
+    target_collection = get_collection_cursor()
+
+    for result in pool.imap_unordered(retrieve_pages, cats):
+        # insert the page pair into database
+        for x in result:
+            if target_collection.count_documents({"English_pageid":x["English_pageid"]}) == 0:
+                target_collection.insert_one(x)
+        # finish one subcategory
+        pbar.update(1)
+    pbar.close()
 
     elapsed_time = time.time() - start
     print(time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
